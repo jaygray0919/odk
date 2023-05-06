3b193d681d0f075edccb88eeffcea46eeb1503ea
jay gray <jaygray0919@gmail.com>
2023-05-06 08:07:34 -0500

    new

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 CURDIR = os.path.abspath(os.path.dirname(__file__))
 
-#sys.path.append(os.path.abspath('exts'))
+sys.path.append(os.path.abspath('exts'))
 
 
 # -- General configuration ------------------------------------------------
